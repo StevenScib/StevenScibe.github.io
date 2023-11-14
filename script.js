4337c2d8982b50fab1243b2925f41094d737c87e
StevenScib <x23174668@student.ncirl.ie>
2023-11-14 20:00:03 +0000

    as

--- script.js
+++ script.js
@@ -3,27 +3,27 @@ function check(playerChoice){
     const computerChoice = choices[Math.floor(Math.random() * 3)];
 
     if (playerChoice === computerChoice) {
-        document.getElementById('result').innerText = "It's a tie!";
+        document.getElementById('winner').innerText = "It's a tie! :/";
     } 
     else if (playerChoice === 'rock') {
         if (computerChoice === 'scissors') {
-            document.getElementById('result').innerText = 'You win! Rock beats scissors.';
+            document.getElementById('winner').innerText = 'You win! Rock beats scissors. :)';
         } else {
-            document.getElementById('result').innerText = 'You lose! Paper beats rock.';
+            document.getElementById('winner').innerText = 'You lose! Paper beats rock. :(';
         }
     } 
     else if (playerChoice === 'paper') {
         if (computerChoice === 'rock') {
-            document.getElementById('result').innerText = 'You win! Paper beats rock.';
+            document.getElementById('winner').innerText = 'You win! Paper beats rock. :)';
         } else {
-            document.getElementById('result').innerText = 'You lose! Scissors beats paper.';
+            document.getElementById('winner').innerText = 'You lose! Scissors beats paper. :(';
         }
     } 
     else if (playerChoice === 'scissors') {
         if (computerChoice === 'paper') {
-            document.getElementById('result').innerText = 'You win! Scissors beats paper.';
+            document.getElementById('winner').innerText = 'You win! Scissors beats paper. :)';
         } else {
-            document.getElementById('result').innerText = 'You lose! Rock beats scissors.';
+            document.getElementById('winner').innerText = 'You lose! Rock beats scissors. :(';
         }
     } 
 }
