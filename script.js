99b223674ac8e368dd58d8d0abb4502d4508ba5d
StevenScib <x23174668@student.ncirl.ie>
2023-11-14 19:38:28 +0000

    FIx

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-function check(){
-    var age;
-    age=parseInt(prompt("What age are you?"));
+function check(playerChoice){
+    const choices = ['rock', 'paper', 'scissors'];
+    const computerChoice = choices[Math.floor(Math.random() * 3)];
 
-    if(age>=18){
-        document.getElementById("placeholder").innerHTML="Welcome";
-    }
-    else if(age<18){
-        document.getElementById("placeholder").innerHTML="Go away! "+age+" is too young";
-    }
-    else{
-        documentGetElementById("placeholder").innerHTML="invalid input, please try again";
-    }
+    if (playerChoice === computerChoice) {
+        document.getElementById('result').innerText = "It's a tie!";
+    } 
+    else if (playerChoice === 'rock') {
+        if (computerChoice === 'scissors') {
+            document.getElementById('result').innerText = 'You win! Rock beats scissors.';
+        } else {
+            document.getElementById('result').innerText = 'You lose! Paper beats rock.';
+        }
+    } 
+    else if (playerChoice === 'paper') {
+        if (computerChoice === 'rock') {
+            document.getElementById('result').innerText = 'You win! Paper beats rock.';
+        } else {
+            document.getElementById('result').innerText = 'You lose! Scissors beats paper.';
+        }
+    } 
+    else if (playerChoice === 'scissors') {
+        if (computerChoice === 'paper') {
+            document.getElementById('result').innerText = 'You win! Scissors beats paper.';
+        } else {
+            document.getElementById('result').innerText = 'You lose! Rock beats scissors.';
+        }
+    } 
 }
